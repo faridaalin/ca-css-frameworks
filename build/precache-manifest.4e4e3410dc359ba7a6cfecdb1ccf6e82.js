c17fec55da1d50d5dbabce6d8dcc901bbe1b16c8
Farida <farida.alin@yahoo.com>
2020-09-11 14:46:23 +0200

    dentation on nav compononent

---
+++ build/precache-manifest.4e4e3410dc359ba7a6cfecdb1ccf6e82.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e02aff2450d5e97ef7f5de7f521784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faae1ba4682e2d91e59f",
+    "url": "/static/css/main.ce0b4cdf.chunk.css"
+  },
+  {
+    "revision": "1d2ce4a2c4cc1e910d2d",
+    "url": "/static/js/2.9e8eb145.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9e8eb145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faae1ba4682e2d91e59f",
+    "url": "/static/js/main.bd2f0bdf.chunk.js"
+  },
+  {
+    "revision": "832c5369209c3ef74e11",
+    "url": "/static/js/runtime-main.5494cfe6.js"
+  },
+  {
+    "revision": "b2422b034b90066e875a236da9a90a95",
+    "url": "/static/media/Accordion-image.b2422b03.png"
+  },
+  {
+    "revision": "a31d3ec8590bd41b18af655d8e6f8601",
+    "url": "/static/media/carousel.a31d3ec8.png"
+  },
+  {
+    "revision": "e529e563fe2a10ba0002e65e033bcb9b",
+    "url": "/static/media/news-1.e529e563.png"
+  },
+  {
+    "revision": "d28607c9cefb4758928e89f745d3b00b",
+    "url": "/static/media/news-2.d28607c9.png"
+  },
+  {
+    "revision": "e33047c8b7ed10fa4f477c916a5cba14",
+    "url": "/static/media/news-3.e33047c8.png"
+  },
+  {
+    "revision": "94c040b7a720a7aebd9d211473b104d6",
+    "url": "/static/media/news-4.94c040b7.png"
+  },
+  {
+    "revision": "15a61f1c1f56c60171f3dca113f2bde8",
+    "url": "/static/media/news-5.15a61f1c.png"
+  },
+  {
+    "revision": "defa8da32f515a2b0a3aeb903085d413",
+    "url": "/static/media/news-6.defa8da3.png"
+  },
+  {
+    "revision": "47d172f4ce5afc84b30dd5d81d70ddc1",
+    "url": "/static/media/news-7.47d172f4.png"
+  },
+  {
+    "revision": "00b2d8c5034c5e298ab36167bdc8103a",
+    "url": "/static/media/news-8.00b2d8c5.png"
+  }
+]);
--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -6,31 +6,52 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import FormControl from "react-bootstrap/FormControl";
 
-import {ButtonStyled, FormStyled, NavWrapper} from './nav.styles';
-
-
+import { ButtonStyled, FormStyled, NavWrapper } from "./nav.styles";
 
 function Navigation() {
   return (
     <NavWrapper>
-    <Navbar bg="light" expand="md" className="navigation px-4 py-3">
-      <Navbar.Brand href="/">The YAY Company</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mt-4 mr-auto mt-md-0 ml-md-5">
-            <NavLink className="nav-link activeClassName  mx-n4 my-2 my-md-0 mx-md-0 mr-md-2 pl-5 py-3 py-md-1 pl-md-4" exact to="/">Home</NavLink>
-            <NavLink className="nav-link activeClassName  mx-n4 my-2 my-md-0 mx-md-0 mr-md-2 pl-5 py-3 py-md-1 pl-md-4" exact to="/news">News</NavLink>
-            <NavLink className="nav-link activeClassName  mx-n4 my-2 my-md-0 mx-md-0 mr-md-2 pl-5 py-3 py-md-1 pl-md-4" exact to="/contact">Contact</NavLink>         
-        </Nav>
+      <Navbar bg="light" expand="md" className="navigation px-4 py-3">
+        <Navbar.Brand href="/">The YAY Company</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mt-4 mr-auto mt-md-0 ml-md-5">
+            <NavLink
+              className="nav-link activeClassName  mx-n4 my-2 my-md-0 mx-md-0 mr-md-2 pl-5 py-3 py-md-1 pl-md-4"
+              exact
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className="nav-link activeClassName  mx-n4 my-2 my-md-0 mx-md-0 mr-md-2 pl-5 py-3 py-md-1 pl-md-4"
+              exact
+              to="/news"
+            >
+              News
+            </NavLink>
+            <NavLink
+              className="nav-link activeClassName  mx-n4 my-2 my-md-0 mx-md-0 mr-md-2 pl-5 py-3 py-md-1 pl-md-4"
+              exact
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </Nav>
 
-         <FormStyled>
+          <FormStyled>
             <Form.Group className="d-flex w-100">
-            <FormControl type="text" placeholder="Search"/>
-            <ButtonStyled variant="primary search" className="input-group-prepend">Search</ButtonStyled>
+              <FormControl type="text" placeholder="Search" />
+              <ButtonStyled
+                variant="primary search"
+                className="input-group-prepend"
+              >
+                Search
+              </ButtonStyled>
             </Form.Group>
-          </FormStyled> 
-      </Navbar.Collapse>
-    </Navbar>
+          </FormStyled>
+        </Navbar.Collapse>
+      </Navbar>
     </NavWrapper>
   );
 }
